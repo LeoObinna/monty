a41fd911806bcc9903d623cbd03dddd61f0e4fd7
Kingsley <louis.officialz@gmail.com>
2023-12-14 07:04:32 -0800

    execute_monty.c

--- monty_main.c
+++ monty_main.c
@@ -1,7 +1,5 @@
 #include "monty.h"
 
-bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
 * main - Entry point for monty interpreter.
 * @argc: Arguments counts passed to the program.
@@ -10,6 +8,8 @@ bus_t bus = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
+	bus_t bus = {NULL, file, NULL, 0};
+
 	char *content;
 	FILE *file;
 	size_t size = 0;
@@ -43,5 +43,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
